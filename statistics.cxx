72124695cd178641eeede19b61581e9c46468dc5
Flo <flo@linux-0hhk.suse>
2015-11-16 16:59:30 +0100

    Hausaufgabe3

--- statistics.cxx
+++ statistics.cxx
@@ -3,17 +3,41 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+void randomarray(double* x, int N)
+{
+    for (int i = 0; i < N; i++) 
+    {
+            x[i]= (double)rand() / (double)RAND_MAX; //erzeugt zahlen zwischen 0 und 1.0 
+    }
+}
+    
+void meanvar (double& mean, double& var, double* x, int N) 
+{
+    for (int i = 0; i < N; i++) 
+    {
+       mean += (x[i])/N;
+    }
+    for (int i = 0; i < N; i++) 
+    {
+        var += pow((x[i] - mean),2)/N;
+    }
+}
 
-int main(){
+int main()
+{
+   srand(time(NULL)); //Seedvalue fuer Zufallsgenerator
    const int N = 100;
    double p[N];
    double mean, var;
-
-   // Some lines here....
-
+    
+   randomarray(p, N);
+   meanvar(mean, var, p, N);
+   
+    for (int i = 0; i < N; i++) 
+    {
+        cout << p[i] << endl;
+    }
+    
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
